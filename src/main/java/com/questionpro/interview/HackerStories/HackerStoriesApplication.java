e4c395684af0abc11894d691701cef68a4865dbe
neeraj7 <kneeraj508@gmail.com>
2020-08-30 15:40:59 +0530

    Initial commit.

---
+++ src/main/java/com/questionpro/interview/HackerStories/HackerStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.questionpro.interview.HackerStories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerStoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/questionpro/interview/HackerStories/HackerStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questionpro.interview.HackerStories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
